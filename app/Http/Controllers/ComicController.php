244cedf183ab07bb967b913c56c36854709f70e4
aletardio <ale.tardio99@gmail.com>
2024-02-20 15:58:44 +0100

    Added function validation to show errors on form

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comic;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -37,19 +38,19 @@ class ComicController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'title' => 'required|max:50|min:3',
-            'thumb' => 'max:255',
-            'price' => 'required|max:10',
-            'series' => 'required|max:50|min:3',
-            'sale_date' => 'required|max:10|min:8',
-            'type' => 'required|max:20',
-            'artists' => 'required',
-            'writers' => 'required',
-            'description' => 'required',
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:50|min:3',
+        //     'thumb' => 'max:255',
+        //     'price' => 'required|max:10',
+        //     'series' => 'required|max:50|min:3',
+        //     'sale_date' => 'required|max:10|min:8',
+        //     'type' => 'required|max:20',
+        //     'artists' => 'required',
+        //     'writers' => 'required',
+        //     'description' => 'required',
+        // ]);
 
-        $form_data = $request->all();
+        $form_data = $this->validation($request->all());
 
         $comic = new Comic();
         $comic->title = $form_data['title'];
@@ -101,34 +102,33 @@ class ComicController extends Controller
     public function update(Request $request, $id)
     {
 
-        $request->validate([
-            'title' => 'required|max:50|min:3',
-            'thumb' => 'max:255',
-            'price' => 'required|max:10',
-            'series' => 'required|max:50|min:3',
-            'sale_date' => 'required|max:10|min:8',
-            'type' => 'required|max:20',
-            'artists' => 'required',
-            'writers' => 'required',
-            'description' => 'required',
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:50|min:3',
+        //     'thumb' => 'max:255',
+        //     'price' => 'required|max:10',
+        //     'series' => 'required|max:50|min:3',
+        //     'sale_date' => 'required|max:10|min:8',
+        //     'type' => 'required|max:20',
+        //     'artists' => 'required',
+        //     'writers' => 'required',
+        //     'description' => 'required'
+        // ]);
 
-        $form_data = $request->all();
+        $form_data = $this->validation($request->all());
 
         $comic = Comic::find($id);
 
+        // $comic->title = $form_data['title'];
+        // $comic->thumb = $form_data['thumb'];
+        // $comic->price = $form_data['price'];
+        // $comic->series = $form_data['series'];
+        // $comic->sale_date = $form_data['sale_date'];
+        // $comic->type = $form_data['type'];
+        // $comic->artists = $form_data['artists'];
+        // $comic->writers = $form_data['writers'];
+        // $comic->description = $form_data['description'];
 
-        $comic->title = $form_data['title'];
-        $comic->thumb = $form_data['thumb'];
-        $comic->price = $form_data['price'];
-        $comic->series = $form_data['series'];
-        $comic->sale_date = $form_data['sale_date'];
-        $comic->type = $form_data['type'];
-        $comic->artists = $form_data['artists'];
-        $comic->writers = $form_data['writers'];
-        $comic->description = $form_data['description'];
-
-        $comic->update();
+        $comic->update($form_data);
 
         return redirect()->route('comics.show', ['comic' => $comic->id]);
     }
@@ -141,6 +141,52 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::find($id);
+
+        $comic->delete();
+
+        return redirect(route("comics.index"));
+    }
+
+    private function validation($data)
+    {
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|max:50|min:3',
+                'thumb' => 'max:255',
+                'price' => 'required|max:10',
+                'series' => 'required|max:50|min:3',
+                'sale_date' => 'required|max:10|min:8',
+                'type' => 'required|max:20',
+                'artists' => 'required|max:50|min:3',
+                'writers' => 'required|max:50|min:3',
+                'description' => 'required'
+            ],
+            [
+                'title.required' => 'Il titolo è obbligatorio',
+                'title.max' => 'Il titolo deve essere al massimo di 50 caratteri',
+                'title.min' => 'Il titolo deve essere al minimo di 3 caratteri',
+                'thumb.max' => 'Il link all\'immagine deve essere al massimo di 255 caratteri',
+                'description.required' => 'La descrizione è obbligatoria',
+                'price.required' => "Il prezzo è obbligatorio",
+                'price.max' => 'I numeri devono essere al massimo di 10 cifre',
+                'series.required' => 'La serie è obbligatoria',
+                'series.max' => 'La serie deve essere al massimo di 50 caratteri',
+                'series.min' => 'La serie deve essere al minimo di 3 caratteri',
+                'sale_date.required' => 'La data di uscita è obbligatoria',
+                'sale_date.max' => 'La data di uscita deve essere al massimo di 10 caratteri',
+                'sale_date.min' => 'La data di uscita deve essere al minimo di 8 caratteri',
+                'type.required' => 'La serie è obbligatoria',
+                'artists.required' => 'Devi inserire almeno un artista',
+                'artists.max' => 'L\'artista deve essere al massimo di 50 caratteri',
+                'artists.min' => 'L\'artista deve essere al minimo di 3 caratteri',
+                'writers.required' => 'Devi inserire almeno uno scrittore',
+                'writers.max' => 'Lo scrittore deve essere al massimo di 50 caratteri',
+                'writers.min' => 'Lo scrittore deve essere al minimo di 3 caratteri'
+            ]
+        )->validate();
+
+        return $validator;
     }
 }
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -7,6 +7,15 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="text-white mt-4">
                     <h2 class="text-center">Edit a comic</h2>
                     <form action="{{ route('comics.update', $comic->id) }}" method="post">
@@ -15,47 +24,55 @@
                         <div class="form-group mb-4">
                             <label for="title">Title</label>
                             <input type="text" name="title" id="title" class="form-control"
-                                placeholder="Insert the title of the comic" value="{{ $comic->title }}" required>
+                                placeholder="Insert the title of the comic" value="{{ old('title') ?? $comic->title }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="thumb">Image</label>
                             <input type="text" name="thumb" id="thumb" class="form-control"
-                                placeholder="Insert the image of the comic" value="{{ $comic->thumb }}" required>
+                                placeholder="Insert the image of the comic" value="{{ old('thumb') ?? $comic->thumb }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="price">Price</label>
                             <input type="text" name="price" id="price" class="form-control"
-                                placeholder="Insert the price of the comic" value="{{ $comic->price }}" required>
+                                placeholder="Insert the price of the comic" value="{{ old('price') ?? $comic->price }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="series">Series</label>
                             <input type="text" name="series" id="series" class="form-control"
-                                placeholder="Insert the series of the comic" value="{{ $comic->series }}" required>
+                                placeholder="Insert the series of the comic" value="{{ old('series') ?? $comic->series }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="type">Type</label>
                             <input type="text" name="type" id="type" class="form-control"
-                                placeholder="Insert the series of the comic" value="{{ $comic->type }}" required>
+                                placeholder="Insert the series of the comic" value="{{ old('type') ?? $comic->type }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="sale_date">Date</label>
                             <input type="text" name="sale_date" id="sale_date" class="form-control"
-                                placeholder="Insert the date of the comic" value="{{ $comic->sale_date }}" required>
+                                placeholder="Insert the date of the comic"
+                                value="{{ old('sale_date') ?? $comic->sale_date }}" required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="artists">Artist</label>
                             <input type="text" name="artists" id="artists" class="form-control"
-                                placeholder="Insert the artist of the comic" value="{{ $comic->artists }}" required>
+                                placeholder="Insert the artist of the comic" value="{{ old('artists') ?? $comic->artists }}"
+                                required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="writers">Writer</label>
                             <input type="text" name="writers" id="writers" class="form-control"
-                                placeholder="Insert the writer of the comic" value="{{ $comic->writers }}" required>
+                                placeholder="Insert the writer of the comic"
+                                value="{{ old('writers') ?? $comic->writers }}" required>
                         </div>
                         <div class="form-group mb-4">
                             <label for="description">Description</label>
                             <textarea name="description" id="description" cols="10" rows="20" placeholder="Description"
-                                class="form-control">{{ $comic->description }}"</textarea>
+                                class="form-control">{{ old('description') ?? $comic->description }}"</textarea>
                         </div>
                         <div class="form-group">
                             <button class="btn-submit" type="submit">
